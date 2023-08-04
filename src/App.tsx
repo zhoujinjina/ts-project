0df931668b2a0dc14df6cabdd43708b605805907
zhoujinjina <3513776811@qq.com>
2023-08-04 20:54:36 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-    </>
-  )
+  const [count, setCount] = useState(0);
+  const addCount = (a:string):never => {
+    setCount(count + 1);
+    throw new Error
+  };
+  return <></>;
 }
 
-export default App
+export default App;
