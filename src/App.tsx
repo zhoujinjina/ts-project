8e82d9374c5d9c0d25609c6b29e24b5b1a2de4af
zhoujinjina <3513776811@qq.com>
2023-08-30 20:26:43 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { FC, useState } from "react";
 import "./App.css";
-
+interface my{
+  name:String;
+  age:number;
+}
 const App: FC = () => {
   const [count, setCount] = useState(0);
-  const add = (): void => {
+  const add = (name:my): void => {
     setCount(count + 1);
   };
   return <>{count}
-  <button onClick={add}>点击</button>
+  <button onClick={()=>add({name:"slice",age:1})}>点击</button>
   </>;
 };
 
